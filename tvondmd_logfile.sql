a902de5ac0b7aa4e03867869d4a0b469c9890c3d
KatyPap <95758186+KatyPap@users.noreply.github.com>
2022-08-06 20:12:19 +0300

    Update tvondmd_logfile.sql

--- tvondmd_logfile.sql
+++ tvondmd_logfile.sql
@@ -6,7 +6,6 @@ CREATE TABLE [dbo].[Events_Log]
 	login_name VARCHAR(50) NOT NULL,
 	event_category VARCHAR(50) NOT NULL,
 	event_date DATETIME NOT NULL,
-	name_of_object VARCHAR(256) NOT NULL,
 	type_of_object VARCHAR(25) NOT NULL,
 	id_of_object INT NOT NULL;
  
