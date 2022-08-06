f737e733fb122b511d644317c1e33b1b01dac120
KatyPap <95758186+KatyPap@users.noreply.github.com>
2022-08-06 20:09:39 +0300

    Update tvondmd_logfile.sql

--- tvondmd_logfile.sql
+++ tvondmd_logfile.sql
@@ -4,13 +4,11 @@ USE moviesondemand;
 CREATE TABLE [dbo].[Events_Log]
 	log_id SMALLINT(5) UNSIGNED NOT NULL,
 	login_name VARCHAR(50) NOT NULL,
-	name_of_database VARCHAR(256) NOT NULL,
 	event_category VARCHAR(50) NOT NULL,
+	event_date DATETIME NOT NULL,
 	name_of_object VARCHAR(256) NOT NULL,
 	type_of_object VARCHAR(25) NOT NULL,
-	id_of_object INT NOT NULL,
-	sql_command VARCHAR(MAX) NOT NULL,
-	event_date DATETIME NOT NULL,
+	id_of_object INT NOT NULL
 ;
  
 ALTER TABLE [dbo].[Events_Log] ADD CONSTRAINT DF_EventsLog_event_date  
